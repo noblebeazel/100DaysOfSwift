29fbee73babc1166acf2944eb8e640db93af97b9
Noble <noblebeazel@gmail.com>
2020-09-15 10:20:20 -0600

    Created day 16 project

---
+++ day16/day16/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day16
+//
+//  Created by Noble Beazel on 9/15/20.
+//  Copyright © 2020 Noble Beazel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ day16/day16Tests/day16Tests.swift
@@ -0,0 +1,34 @@
+//
+//  day16Tests.swift
+//  day16Tests
+//
+//  Created by Noble Beazel on 9/15/20.
+//  Copyright © 2020 Noble Beazel. All rights reserved.
+//
+
+import XCTest
+@testable import day16
+
+class day16Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
